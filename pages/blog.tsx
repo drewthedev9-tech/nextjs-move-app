754e2a60e886b5f0920b3f441ec34cbe18ac9c78
unknown <you@example.com>
2021-01-02 13:45:12 +1300

    added

---
+++ pages/blog.tsx
@@ -0,0 +1,64 @@
+import Link from 'next/link'
+import Container from "react-bootstrap/Container"
+import Card from "../components/card"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import axios from "axios";
+// import Header from "../components/header" 
+
+
+// get static props
+function About(props) {
+   console.log(props.movies)
+  return (
+      <div>
+      <Container fluid="md">
+      <h2 >The Blog page</h2>
+    
+            <Row>
+                <Col style={{justifyContent:'center'}}>
+                <h2>{props.movies.title}</h2>
+                This is the blog page Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime praesentium culpa omnis accusamus vero nam error aliquid ratione fugit distinctio accusantium, mollitia explicabo, magni pariatur possimus enim amet. Vero, eius!
+                </Col>
+            </Row>
+        </Container>
+      
+      </div>
+  )
+}
+
+export default About
+
+// export async function getStaticProps(){
+//   // using dotenv and getting the url from env file.
+//   const {API_URL} = process.env
+
+//   const response = await axios.get(`${API_URL}/pages`)
+  
+//   return {
+//     props: {
+//         pages: response.data[0]
+//     }
+// }
+
+// }
+
+export async function getStaticProps(){
+  // using dotenv and getting the url from env file.
+  const {API_URL} = process.env
+
+  // const res = await fetch(`${API_URL}/movies`)
+  // const response = await res.json()
+  const response = await axios.get(`${API_URL}/movies`)
+  
+ 
+
+  return {
+    props: {
+        movies: response.data[0]
+    }
+}
+
+}
+
+
---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+
+import axios from "axios";
+import Card from "../components/card"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import fetch from 'isomorphic-unfetch'
+// style={{display:'flex', justifyContent:'center', margin:'20px'}}
+const Home=(props)=> {
+
+   console.log(props.movies)
+  return (
+    <Container fluid>
+      <Row style={{justifyContent:'center'}}>
+      {props.movies.map(movie=>(
+        <div style={{margin:'2rem'}} >
+            <Card
+            key={movie.id}
+            movie={movie}
+            />
+        </div>
+      ))}
+      </Row>
+    
+ </Container>
+  )
+}
+
+export default Home
+
+export async function getStaticProps(){
+  // using dotenv and getting the url from env file.
+  const {API_URL} = process.env
+
+  // const res = await fetch(`${API_URL}/movies`)
+  // const response = await res.json()
+  const response = await axios.get(`${API_URL}/movies`)
+  
+ 
+
+  return {
+    props: {
+        movies: response.data
+    }
+}
+
+}
+
